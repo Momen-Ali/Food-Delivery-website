712537164c0043a3ac0677dbb3ec2116813abc5d
Caff <114318889+Momen-Ali@users.noreply.github.com>
2022-12-15 20:37:58 +0200

    Update CartContainer.jsx

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -70,7 +70,7 @@ const CartContainer = () => {
               ))}
           </div>
           {/* Total Section */}
-          <div className="w-full flex-1 bg-cartTotal rounded-tl-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
+          <div className="w-full flex-1 bg-cartTotal rounded-tl-[2rem] flex flex-col items-center justify-evenly px-8 pt-9 pb-20">
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-400 text-lg">Sub Total</p>
               <p className="text-gray-400 text-lg">$ {tot}</p>
